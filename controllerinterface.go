78fc3676260349a9d76cf98b049cd959222bece1
aliforever <alipadida@live.com>
2021-10-26 01:30:48 +0330

    changes

---
+++ controllerinterface.go
@@ -0,0 +1,12 @@
+package httprouter
+
+import "net/http"
+
+type Controller interface {
+	Path() string
+	GET(http.ResponseWriter, *http.Request)
+	POST(http.ResponseWriter, *http.Request)
+	PATCH(http.ResponseWriter, *http.Request)
+	PUT(http.ResponseWriter, *http.Request)
+	DELETE(http.ResponseWriter, *http.Request)
+}
--- httprouter.go
+++ httprouter.go
@@ -39,3 +39,49 @@ func (router *Router) Register(handler http.Handler, path string, methods ...str
 	router.routers[path] = p
 	return
 }
+
+func (router *Router) GET(path string, handler http.HandlerFunc) (err error) {
+	err = router.Register(handler, path, "GET")
+	return
+}
+
+func (router *Router) POST(path string, handler http.HandlerFunc) (err error) {
+	err = router.Register(handler, path, "POST")
+	return
+}
+
+func (router *Router) PUT(path string, handler http.HandlerFunc) (err error) {
+	err = router.Register(handler, path, "PUT")
+	return
+}
+
+func (router *Router) PATCH(path string, handler http.HandlerFunc) (err error) {
+	err = router.Register(handler, path, "PATCH")
+	return
+}
+
+func (router *Router) DELETE(path string, handler http.HandlerFunc) (err error) {
+	err = router.Register(handler, path, "DELETE")
+	return
+}
+
+func (router *Router) Controller(controller Controller) (err error) {
+	err = router.Register(http.HandlerFunc(controller.POST), controller.Path(), "POST")
+	if err != nil {
+		return
+	}
+	err = router.Register(http.HandlerFunc(controller.GET), controller.Path(), "GET")
+	if err != nil {
+		return
+	}
+	err = router.Register(http.HandlerFunc(controller.PATCH), controller.Path(), "PATCH")
+	if err != nil {
+		return
+	}
+	err = router.Register(http.HandlerFunc(controller.PUT), controller.Path(), "PUT")
+	if err != nil {
+		return
+	}
+	err = router.Register(http.HandlerFunc(controller.DELETE), controller.Path(), "DELETE")
+	return
+}
