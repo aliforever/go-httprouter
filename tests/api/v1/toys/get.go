491559c643a0a237bfe962b346a4c37ae5bb2b1c
aliforever <alipadida@live.com>
2021-10-27 11:48:28 +0330

    changes

--- tests/api/v1/toys/get.go
+++ tests/api/v1/toys/get.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/aliforever/go-httprouter"
 )
@@ -10,12 +12,52 @@ import (
 func main() {
 	var router httprouter.Router
 
-	err := router.Register(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+	expected := "This is toys handler"
 
+	err := router.Register(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(expected))
 	}))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	go func() {
+		err := http.ListenAndServe("localhost:8080", router)
+		if err != nil {
+			return
+		}
+	}()
+
+	time.Sleep(time.Second * 2)
+
+	resp, err := testToys()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if resp != expected {
+		fmt.Printf("Test Failed.\nExpected: %s\nActual: %s", expected, resp)
+		return
+	}
+
+	fmt.Printf("Test Passed.")
+}
+
+func testToys() (response string, err error) {
+	var resp *http.Response
+	resp, err = http.Get("http://localhost:8080/v1/toys")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	var b []byte
+	b, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	response = string(b)
+	return
 }
